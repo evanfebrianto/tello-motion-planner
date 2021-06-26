be825d0d7c2f3150ec991c4b79ad3a2d9c2effab
evanfebrianto <van.evanfebrianto@gmail.com>
2021-06-26 21:01:36 +0800

    first commit

---
+++ record-video.py
@@ -0,0 +1,49 @@
+import time, cv2
+from threading import Thread
+from djitellopy import Tello
+
+tello = Tello()
+
+tello.connect()
+
+keepRecording = True
+tello.streamon()
+frame_read = tello.get_frame_read()
+
+def videoRecorder():
+    # create a VideoWrite object, recoring to ./video.avi
+    height, width, _ = frame_read.frame.shape
+    video = cv2.VideoWriter('video.avi', cv2.VideoWriter_fourcc(*'XVID'), 30, (width, height))
+
+    while keepRecording:
+        video.write(frame_read.frame)
+        time.sleep(1 / 30)
+
+    video.release()
+
+# we need to run the recorder in a seperate thread, otherwise blocking options
+#  would prevent frames from getting added to the video
+recorder = Thread(target=videoRecorder)
+recorder.start()
+
+print('Tello battery percentage: {}'.format(tello.get_battery()))
+tello.takeoff()
+# tello.move_left(20)
+# tello.rotate_counter_clockwise(360)
+time.sleep(5)
+
+start = time.time()
+while time.time() - start < 3: #second
+    tello.send_rc_control(left_right_velocity=0,
+                        forward_backward_velocity=0,
+                        up_down_velocity=2,
+                        yaw_velocity=20)
+
+    # tello.curve_xyz_speed(x1=100, y1=100, z1=100,
+    #                       x2=200, y2=200, z2=200,
+    #                       speed=10)
+
+tello.land()
+
+keepRecording = False
+recorder.join()
